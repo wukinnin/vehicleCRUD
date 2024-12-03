3779410f49c2bae64ed8705c9deaddddae1159d2
wukinnin <nimugonzaga@gmail.com>
2024-12-03 16:17:27 +0800

    install livewire and components

---
+++ app/Livewire/Posts.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use App\Models\Post;
+
+class Posts extends Component
+{
+    public $posts, $make, $model, $post_id;
+    public $updateMode = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+
+    public function render()
+    {
+        $this->posts = Post::all();
+        return view('livewire.posts');
+
+    }
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+
+    private function resetInputFields(){
+        $this->make = '';
+        $this->model = '';
+    }
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+
+    public function store()
+    {
+        $validatedDate = $this->validate([
+            'make' => 'required',
+            'model' => 'required',
+        ]);
+
+        Post::create($validatedDate);
+        session()->flash('message', 'Post Created Successfully.');
+        $this->resetInputFields();
+    }
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+
+    public function edit($id)
+    {
+        $post = Post::findOrFail($id);
+        $this->post_id = $id;
+        $this->make = $post->make;
+        $this->model = $post->model;
+        $this->updateMode = true;
+    }
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+
+    public function cancel()
+    {
+        $this->updateMode = false;
+        $this->resetInputFields();
+    }
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+
+    public function update()
+    {
+        $validatedDate = $this->validate([
+            'title' => 'required',
+            'model' => 'required',
+        ]);
+
+        $post = Post::find($this->post_id);
+
+        $post->update([
+            'make' => $this->make,
+            'model' => $this->model,
+
+        ]);
+
+        $this->updateMode = false;
+        session()->flash('message', 'Post Updated Successfully.');
+        $this->resetInputFields();
+    }
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+
+    public function delete($id)
+    {
+        Post::find($id)->delete();
+        session()->flash('message', 'Post Deleted Successfully.');
+    }
+}
