983353c2bd55f5b17dc27acb1b413e34f6b24a74
wukinnin <nimugonzaga@gmail.com>
2024-12-10 09:34:26 +0800

    offline bootstrap capability

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <title>Vehicle List</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.css') }}"><!-- located at public\bootstrap\css-->
     @livewireStyles
 </head>
 
